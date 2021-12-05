056c5a4dfc63b764d5e0557677cf909935e52b33
SteRap <92691510+SteRap@users.noreply.github.com>
2021-12-05 09:59:41 +0100

    add title

--- src/Register.js
+++ src/Register.js
@@ -44,76 +44,82 @@ function Register(props) {
     }
   }
   return (
-    <article
-      className="br3 ba b--black-20 mt6-l mt6-m mt5 w-90 w-50-m w-25-l mw6 mh3  center form"
-      onKeyDown={onEnterSubmitRegister}
-    >
-      <main
-        className="pa4"
-        action="sign-up_submit"
-        method="get"
-        acceptCharset="utf-8"
+    <div>
+      <div className="tc">
+        <h3 className="title-h3">Let's play</h3>
+        <h1 className="title-h1">Quizzical</h1>
+      </div>
+      <article
+        className="br3 ba b--black-20 w-90 w-50-m w-25-l mw6 mh3 center form"
+        onKeyDown={onEnterSubmitRegister}
       >
-        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-          <legend className="center f2 ph0 mh0 fw6">Register</legend>
-          <div>
-            <label className="db fw4 lh-copy f5 mt3" htmlFor="username">
-              Username
-            </label>
+        <main
+          className="pa4"
+          action="sign-up_submit"
+          method="get"
+          acceptCharset="utf-8"
+        >
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="center f2 ph0 mh0 fw6">Register</legend>
+            <div>
+              <label className="db fw4 lh-copy f5 mt3" htmlFor="username">
+                Username
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent w-100 measure br2"
+                type="text"
+                name="username"
+                id="username"
+                onChange={onSetUsername}
+              />
+            </div>
+            <div className="mt3">
+              <label className="db fw4 lh-copy f5" htmlFor="email-address">
+                Email address
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent w-100 measure br2"
+                type="email"
+                name="email-address"
+                id="email-address"
+                onChange={onSetEmail}
+              />
+            </div>
+            <div className="mt3">
+              <label className="db fw4 lh-copy f5" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent w-100 measure br2"
+                type="password"
+                name="password"
+                id="password"
+                onChange={onSetPassword}
+              />
+            </div>
+          </fieldset>
+          <div className="flex justify-between items-center mt2">
             <input
-              className="pa2 input-reset ba bg-transparent w-100 measure br2"
-              type="text"
-              name="username"
-              id="username"
-              onChange={onSetUsername}
+              className="b ph3 pv2 input-reset ba b--black grow pointer f5 br2 form-button"
+              type="submit"
+              value="Register"
+              onClick={onSubmitRegister}
             />
+            <p
+              onClick={() => props.routeChange("signIn")}
+              className=" lh-copy f6 link dim black db pointer"
+            >
+              Sign In
+            </p>
           </div>
-          <div className="mt3">
-            <label className="db fw4 lh-copy f5" htmlFor="email-address">
-              Email address
-            </label>
-            <input
-              className="pa2 input-reset ba bg-transparent w-100 measure br2"
-              type="email"
-              name="email-address"
-              id="email-address"
-              onChange={onSetEmail}
-            />
-          </div>
-          <div className="mt3">
-            <label className="db fw4 lh-copy f5" htmlFor="password">
-              Password
-            </label>
-            <input
-              className="pa2 input-reset ba bg-transparent w-100 measure br2"
-              type="password"
-              name="password"
-              id="password"
-              onChange={onSetPassword}
-            />
-          </div>
-        </fieldset>
-        <div className="flex justify-between items-center mt2">
-          <input
-            className="b ph3 pv2 input-reset ba b--black grow pointer f5 br2 form-button"
-            type="submit"
-            value="Register"
-            onClick={onSubmitRegister}
-          />
-          <p
-            onClick={() => props.routeChange("signIn")}
-            className=" lh-copy f6 link dim black db pointer"
-          >
-            Sign In
-          </p>
-        </div>
-        {registrationOff && (
-          <div className="registration-Off">
-            Unable to register user. <br /> Please, fill all the input fields.
-          </div>
-        )}
-      </main>
-    </article>
+          {registrationOff && (
+            <div className="registration-Off">
+              Unable to register user. <br /> Please, fill all the input fields.
+            </div>
+          )}
+        </main>
+      </article>
+    </div>
   );
 }
 export default Register;
--- src/Signin.js
+++ src/Signin.js
@@ -40,64 +40,70 @@ function SignIn(props) {
   }
 
   return (
-    <article
-      className="br3 ba b--black-20 mt6-l mt6-m mt6 w-90 w-50-m w-25-l mw6 mh3  center form"
-      onKeyDown={onEnterSubmitSignin}
-    >
-      <main
-        className="pa4"
-        action="sign-up_submit"
-        method="get"
-        acceptCharset="utf-8"
+    <div>
+      <div className="tc">
+        <h3 className="title-h3">Let's play</h3>
+        <h1 className="title-h1">Quizzical</h1>
+      </div>
+      <article
+        className="br3 ba b--black-20 w-90 w-50-m w-25-l mw6 mh3 center form"
+        onKeyDown={onEnterSubmitSignin}
       >
-        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-          <legend className=" center f2 ph0 mh0 fw6">Sign In</legend>
-          <div>
-            <label className="db fw4 lh-copy f5 mt3" htmlFor="email-address">
-              Email address
-            </label>
+        <main
+          className="pa4"
+          action="sign-up_submit"
+          method="get"
+          acceptCharset="utf-8"
+        >
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className=" center f2 ph0 mh0 fw6">Sign In</legend>
+            <div>
+              <label className="db fw4 lh-copy f5 mt3" htmlFor="email-address">
+                Email address
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent w-100 measure br2"
+                type="email"
+                name="email-address"
+                id="email-address"
+                onChange={onEmailChange}
+              />
+            </div>
+            <div className="mt3">
+              <label className="db fw4 lh-copy f5" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent w-100 measure br2"
+                type="password"
+                name="password"
+                id="password"
+                onChange={onPasswordChange}
+              />
+            </div>
+          </fieldset>
+          <div className="flex justify-between items-center mt3">
             <input
-              className="pa2 input-reset ba bg-transparent w-100 measure br2"
-              type="email"
-              name="email-address"
-              id="email-address"
-              onChange={onEmailChange}
+              className="b ph3 pv2 input-reset ba b--black grow pointer f5 br2 form-button"
+              type="submit"
+              value="Sign In"
+              onClick={onSubmitSignin}
             />
+            <p
+              onClick={() => props.routeChange("register")}
+              className="f6 lh-copy link dim black db pointer"
+            >
+              Register
+            </p>
           </div>
-          <div className="mt3">
-            <label className="db fw4 lh-copy f5" htmlFor="password">
-              Password
-            </label>
-            <input
-              className="pa2 input-reset ba bg-transparent w-100 measure br2"
-              type="password"
-              name="password"
-              id="password"
-              onChange={onPasswordChange}
-            />
-          </div>
-        </fieldset>
-        <div className="flex justify-between items-center mt3">
-          <input
-            className="b ph3 pv2 input-reset ba b--black grow pointer f5 br2 form-button"
-            type="submit"
-            value="Sign In"
-            onClick={onSubmitSignin}
-          />
-          <p
-            onClick={() => props.routeChange("register")}
-            className="f6 lh-copy link dim black db pointer"
-          >
-            Register
-          </p>
-        </div>
-        {wrongCredentials && (
-          <div className="signin-credentials">
-            You inserted wrong credentials. <br /> Please try again!
-          </div>
-        )}
-      </main>
-    </article>
+          {wrongCredentials && (
+            <div className="signin-credentials">
+              You inserted wrong credentials. <br /> Please try again!
+            </div>
+          )}
+        </main>
+      </article>
+    </div>
   );
 }
 export default SignIn;
