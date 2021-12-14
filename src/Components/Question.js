4b78a9820ad5937939f3caedefd8126b40f97d0c
SteRap <92691510+SteRap@users.noreply.github.com>
2021-12-14 11:12:48 +0100

    restructure working tree

---
+++ src/Components/Question.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Base64 } from "js-base64";
+
+function Question(props) {
+  return (
+    <div>
+      <h2 className="main-question" id={props.id}>
+        {Base64.decode(props.question)}
+      </h2>
+    </div>
+  );
+}
+
+export default Question;
---
+++ src/Containers/Answer.js
@@ -0,0 +1,56 @@
+import React from "react";
+import AnswersButtons from "../Components/AnswersButtons";
+
+function Answer(props) {
+  const answersArray = [];
+  const [answer, setAnswer] = React.useState(answersArray);
+
+  React.useEffect(() => {
+    setAnswer(props.data);
+  }, [props.data]);
+
+  React.useEffect(() => {
+    shuffle(props.data);
+    createAnswersArray();
+  }, []);
+
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+    return array;
+  }
+
+  function createAnswersArray() {
+    answersArray.push(
+      props.data[0],
+      props.data[1],
+      props.data[2],
+      props.data[3]
+    );
+    return answersArray;
+  }
+
+  const answersButtons = answer.map((option) => (
+    <AnswersButtons
+      answer={option.answer}
+      key={option.id}
+      isHeld={option.isHeld}
+      id={option.id}
+      checked={option.checked}
+      handleChange={props.handleChange}
+      isWrong={option.isWrong}
+    />
+  ));
+
+  return <div className="main-answers">{answersButtons}</div>;
+}
+
+export default Answer;
